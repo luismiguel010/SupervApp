cb129e1ba6b6c14e5e6578bdfcfb2a838e41d567
Luis Miguel <luismiguel010@gmail.com>
2021-03-04 22:03:28 -0500

    Se añade botón de añadir proyecto en el activity principal

--- app/src/main/java/co/com/luis/supervapp/MainActivity.java
+++ app/src/main/java/co/com/luis/supervapp/MainActivity.java
@@ -1,14 +1,50 @@
 package co.com.luis.supervapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton mAddFab, mAddAlarmFab;
+    TextView addAlarmActionText;
+    Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAddFab = findViewById(R.id.add_fab);
+        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
+        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
+        mAddAlarmFab.setVisibility(View.GONE);
+        addAlarmActionText.setVisibility(View.GONE);
+        isAllFabsVisible = false;
+        mAddFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!isAllFabsVisible) {
+                            mAddAlarmFab.show();
+                            addAlarmActionText.setVisibility(View.VISIBLE);
+                            isAllFabsVisible = true;
+                        } else {
+                            mAddAlarmFab.hide();
+                            addAlarmActionText.setVisibility(View.GONE);
+                            isAllFabsVisible = false;
+                        }
+                    }
+                });
+
+        mAddAlarmFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Alarm Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
