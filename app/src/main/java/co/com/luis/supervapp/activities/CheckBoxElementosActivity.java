1c77eedf97b3ef0af15d7097eb75500e67cec3bb
luismiguel010 <luismiguel010@gmail.com>
2021-04-02 13:41:11 -0500

    Se añaden activity para el checkbox de elementos y se agregan elementos a los string values

---
+++ app/src/main/java/co/com/luis/supervapp/activities/CheckBoxElementosActivity.java
@@ -0,0 +1,16 @@
+package co.com.luis.supervapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.com.luis.supervapp.R;
+
+public class CheckBoxElementosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_box_elementos);
+    }
+}
